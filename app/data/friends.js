73444020847004648779c650aa3a9bfbd0673143
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-09-12 20:38:32 -0400

    updated data list

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,9 +13,41 @@ let friends = [
         "4",
         "2",
         "4",
-        "",
+        "3",
         "1",
         "4",
+        "5"
+        ]
+    },
+    {
+        "name": "Elmo",
+        "photo": "https://target.scene7.com/is/image/Target/52148551?wid=488&hei=488&fmt=pjpeg",
+        "scores": [
+        "1",
+        "3",
+        "2",
+        "5",
+        "3",
+        "1",
+        "2",
+        "4",
+        "4",
+        "5"
+        ]
+    },
+    {
+        "name": "Bill Gates",
+        "photo": "https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X.jpg",
+        "scores": [
+        "5",
+        "5",
+        "4",
+        "4",
+        "5",
+        "5",
+        "4",
+        "4",
+        "5",
         "5"
         ]
     }
